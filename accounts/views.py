9ac114c431d20478d83fff02cbae1a02019acd94
Maurilio Bueno <53921060+Buenomaurilio@users.noreply.github.com>
2025-09-22 16:36:32 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -22,7 +22,7 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         if self.request.user.is_superadmin:
             return reverse('admin_dashboard')
-        return reverse('appointments_list')    
+        return reverse('appointment_list')    
         # return reverse('dashboard')
     # def get_success_url(self):
     #     user = self.request.user
